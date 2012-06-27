41733a753aee8fda801287997195f2d8188f25db
Dan Rosen <dan@marakana.com>
2012-06-27 14:56:20 -0700

    respond to GET requests with a "name" path param

--- src/main/java/chirp/resources/HelloResource.java
+++ src/main/java/chirp/resources/HelloResource.java
@@ -2,13 +2,15 @@ package chirp.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/hello")
 public class HelloResource {
 
 	@GET
-	public String hello() {
-		return "Hello.";
+	@Path("{name}")
+	public String hello(@PathParam("name") String name) {
+		return "Hello, " + name;
 	}
 
 }
