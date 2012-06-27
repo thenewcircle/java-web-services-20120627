ce09e69a09b38089d0423f736569c5f1ade3e796
Dan Rosen <dan@marakana.com>
2012-06-27 15:42:28 -0700

    Added proper HTTP response codes

--- src/main/java/chirp/resources/HelloResource.java
+++ src/main/java/chirp/resources/HelloResource.java
@@ -7,6 +7,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/hello")
 public class HelloResource {
@@ -15,14 +19,22 @@ public class HelloResource {
 
 	@GET
 	@Path("{name}")
-	public String hello(@PathParam("name") String name) {
-		return "Hello, " + (users.contains(name) ? name : "UNKNOWN");
+	public Response hello(@PathParam("name") String name) {
+		if (users.contains(name)) {
+			return Response.ok("Hello, " + name).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
 	}
 
 	@PUT
 	@Path("{name}")
-	public String putUser(@PathParam("name") String name) {
-		users.add(name);
-		return "Accepted " + name;
+	public Response putUser(@PathParam("name") String name, @Context UriInfo info) {
+		if (users.contains(name)) {
+			return Response.status(Status.NO_CONTENT).build();
+		} else {
+			users.add(name);
+			return Response.created(info.getRequestUri()).build();
+		}
 	}
 }
