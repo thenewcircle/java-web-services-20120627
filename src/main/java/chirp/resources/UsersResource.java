b9b39abf54d151bdd32abe5e8d23ac3d380c5ee1
Dan Rosen <dan@marakana.com>
2012-06-27 17:32:18 -0700

    implemented more useful user service

--- src/main/java/chirp/resources/UsersResource.java
+++ src/main/java/chirp/resources/UsersResource.java
@@ -1,48 +1,53 @@
 package chirp.resources;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.UriBuilder;
 
 @Path("/users")
 public class UsersResource {
 
-	private static Set<String> users = new HashSet<String>();
+	private static Map<String, String> users = new HashMap<String, String>();
 
 	@GET
-	@Path("{name}")
-	public Response hello(@PathParam("name") String name) {
-		if (users.contains(name)) {
-			return Response.ok("Hello, " + name).build();
+	@Path("{username}")
+	public Response getUser(@PathParam("username") String username) {
+		String realname = users.get(username);
+		if (realname != null) {
+			return Response.ok(realname).build();
 		} else {
 			return Response.status(Status.NOT_FOUND).build();
 		}
 	}
 
-	@PUT
-	@Path("{name}")
-	public Response putUser(@PathParam("name") String name, @Context UriInfo info) {
-		if (users.contains(name)) {
-			return Response.noContent().build();
+	@POST
+	public Response putUser(@FormParam("username") String username, @FormParam("realname") String realname) {
+		if (users.containsKey(username)) {
+			return Response.status(Status.FORBIDDEN).build();
 		} else {
-			users.add(name);
-			return Response.created(info.getRequestUri()).build();
+			users.put(username, realname);
+			URI uri =
+					UriBuilder
+					.fromPath(username)
+					.build();
+			return Response.created(uri).build();
 		}
 	}
 
 	@DELETE
-	@Path("{name}")
-	public Response deleteUser(@PathParam("name") String name) {
-		if (users.remove(name)) {
+	@Path("{username}")
+	public Response deleteUser(@PathParam("username") String username) {
+		if (users.remove(username) != null) {
 			return Response.noContent().build();
 		} else {
 			return Response.status(Status.NOT_FOUND).build();
