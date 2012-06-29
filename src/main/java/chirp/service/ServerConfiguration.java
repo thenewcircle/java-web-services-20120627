a6583fd414002cb7db989913895da1a80cb9f5b0
Dan Rosen <dan@marakana.com>
2012-06-29 15:05:18 -0700

    added jersey linking filter

--- src/main/java/chirp/service/ServerConfiguration.java
+++ src/main/java/chirp/service/ServerConfiguration.java
@@ -7,8 +7,17 @@ import javax.ws.rs.ext.Provider;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.api.core.ResourceConfigurator;
+import com.sun.jersey.server.linking.LinkFilter;
+
 @Provider
-public class ServerConfiguration implements ContextResolver<ObjectMapper> {
+public class ServerConfiguration implements ResourceConfigurator, ContextResolver<ObjectMapper> {
+
+	@SuppressWarnings("unchecked")
+	public void configure(ResourceConfig config) {
+		config.getContainerResponseFilters().add(LinkFilter.class);
+	}
 
 	public ObjectMapper getContext(Class<?> type) {
 		return new ObjectMapper().setSerializationInclusion(NON_NULL);
